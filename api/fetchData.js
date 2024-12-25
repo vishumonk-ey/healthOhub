d76d59d26e73ce428c6a0cb1e2a51c541d923dcd
vishumonk-ey <vkant2005@gmail.com>
2024-12-26 00:26:09 +0530

    Update fetchData.js

--- api/fetchData.js
+++ api/fetchData.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   const apiKey = process.env.API_KEY; // Access the hidden API key
   const apiUrl = process.env.ENDPOINT; // External API URL without query parameters for POST
 
